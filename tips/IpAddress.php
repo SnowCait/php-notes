8bae79d8ac5be53c8a923f63d2509b649abb7275
雪猫 <SnowCait@users.noreply.github.com>
2019-06-28 19:06:11 +0900

    Update IpAddress.php

--- tips/IpAddress.php
+++ tips/IpAddress.php
@@ -8,28 +8,28 @@ declare(strict_types=1);
  */
 function in_ip_address_range(string $needle_ip, array $ranges): bool
 {
-  foreach ($ranges as $range)
-  {
-    [$haystack_ip, $mask] = explode('/', $range);  // [PHP < 7] list($accept_ip, $mask) = 
-    $haystack_long = ip2long($haystack_ip) >> (32 - $mask);
-    $needle_long = ip2long($needle_ip) >> (32 - $mask);
-    if ($haystack_long === $needle_long)
+    foreach ($ranges as $range)
     {
-      return true;
+        [$haystack_ip, $mask] = explode('/', $range);  // [PHP < 7] list($accept_ip, $mask) = 
+        $haystack_long = ip2long($haystack_ip) >> (32 - $mask);
+        $needle_long = ip2long($needle_ip) >> (32 - $mask);
+        if ($haystack_long === $needle_long)
+        {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 $ip = $_SERVER['REMOTE_ADDR'];
 $private_ips = [
-  '10.0.0.0/8',
-  '172.16.0.0/12',
-  '192.168.0.0/16',
+    '10.0.0.0/8',
+    '172.16.0.0/12',
+    '192.168.0.0/16',
 ];
 if (in_ip_address_range($ip, $private_ips))
 {
-  echo "{$ip} is private network IP address.";
+    echo "{$ip} is private network IP address.";
 }
 
 // 参考
